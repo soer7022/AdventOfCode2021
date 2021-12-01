986155ff72ddb61dc480b8f7b33384eff9e9fae7
Søren Eriksen <soeren.skov.eriksen@lego.com>
2021-12-01 06:11:35 +0100

    Finish day 1

--- 1/1_part1.py
+++ 1/1_part1.py
@@ -1,4 +1,10 @@
 with open('input.txt') as f:
     data = [int(i) for i in f.readlines()]
+count = 0
+prev = 0
+for i in data:
+    if i > prev != 0:
+        count += 1
+    prev = i
 
-print(data)
+print(count)
---
+++ 1/1_part2.py
@@ -0,0 +1,16 @@
+with open('input.txt') as f:
+    data = [int(i) for i in f.readlines()]
+
+
+count = 0
+prev = data[0] + data[1] + data[2]
+
+for i in range(len(data)):
+    try:
+        curr = data[i] + data[i + 1] + data[i + 2]
+    except IndexError:
+        break
+    if curr > prev:
+        count += 1
+    prev = curr
+print(count)
