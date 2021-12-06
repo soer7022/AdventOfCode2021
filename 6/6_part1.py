05af71324a9c0e83dcc893678a8d6193e7259dbb
Søren Eriksen <soeren.skov.eriksen@lego.com>
2021-12-06 10:33:39 +0100

    day 6

---
+++ 6/6_part1.py
@@ -0,0 +1,12 @@
+with open('input.txt') as f:
+    data = [int(n) for n in f.readline().split(",")]
+
+for i in range(80):
+    for index, fish in enumerate(data):
+        if fish == 0:
+            data.append(9)
+            data[index] = 6
+        else:
+            data[index] -= 1
+    print(i)
+print(len(data))
---
+++ 6/6_part2.py
@@ -0,0 +1,17 @@
+from functools import cache
+
+with open('input.txt') as f:
+    data = [int(n) for n in f.readline().split(",")]
+
+
+@cache
+def simulate_fish(timer, days_remaining):
+    if days_remaining <= 0:
+        return 1
+    elif timer == 0:
+        return simulate_fish(6, days_remaining - 1) + simulate_fish(8, days_remaining - 1)
+    else:
+        return simulate_fish(timer - 1, days_remaining - 1)
+
+
+print(sum(simulate_fish(t, 256) for t in data))
